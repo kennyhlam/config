43e0ce59c9c734a20397cf01a2d8b8b2a109044d
Kenny Lam <khl1392@gmail.com>
2015-02-15 22:04:16 -0800

    updates

--- ubuntu-config.sh
+++ ubuntu-config.sh
@@ -78,31 +78,57 @@ if [ $(whoami) != 'vagrant' ]; then # a base box, not vm
 	cd $HOME/Downloads
 	wget https://github.com/sobstel/SyncedSideBar/archive/master.zip
 	unzip master.zip && mv SyncedSideBar-master ~/.config/sublime-text-2/Packages/SyncedSideBar
+	cd $HOME/Downloads
+    rm master.zip
 	# Focus file sidebar
 	cd $HOME/Downloads
 	wget https://github.com/miguelgraz/FocusFileOnSidebar/archive/master.zip
 	unzip master.zip && mv FocusFileOnSidebar-master ~/.config/sublime-text-2/Packages/FocusFileOnSidebar
+	cd $HOME/Downloads
+    rm master.zip
 	# All Autocomplete Sublime Text
 	cd $HOME/Downloads
 	wget https://github.com/alienhard/SublimeAllAutocomplete/archive/master.zip
 	unzip master.zip && mv SublimeAllAutocomplete-master ~/.config/sublime-text-2/Packages/SublimeAllAutocomplete
+	cd $HOME/Downloads
+    rm master.zip
 	# Sublimetext markdown preview
 	cd $HOME/Downloads
 	wget https://github.com/revolunet/sublimetext-markdown-preview/archive/master.zip
 	unzip master.zip && mv sublimetext-markdown-preview-master ~/.config/sublime-text-2/Packages/sublimetext-markdown-preview
+	cd $HOME/Downloads
+    rm master.zip
 	# Sublime terminal
 	cd $HOME/Downloads
 	wget https://github.com/wbond/sublime_terminal/archive/master.zip
 	unzip master.zip && mv sublime_terminal-master ~/.config/sublime-text-2/Packages/sublime_terminal
+	cd $HOME/Downloads
+    rm master.zip
 	# GitGutter
 	cd $HOME/Downloads
 	wget https://github.com/jisaacks/GitGutter/archive/master.zip
 	unzip master.zip && mv GitGutter-master ~/.config/sublime-text-2/Packages/GitGutter
-	
+	cd $HOME/Downloads
+    rm master.zip
+
+    # let package control run and update
+    /usr/bin/sublime&
+    sleep(5)
+    killall sublime
+    /usr/bin/sublime&
+    sleep(5)
+    killall sublime
+
+    sudo sh -c "curl -v https://raw.githubusercontent.com/kennyhlam/config/master/sublime.desktop --output /usr/share/applications/sublime.desktop" 
+    sudo sed -i -e s/gedit/sublime/ defaults.list
+    # must be run after sublime has initialized
 	cd ~/.config/sublime-text-2/Packages/User
+    rm Preferences.sublime-settings
+    rm GitGutter.sublime-settings
+    rm Default\ \(Linux\).sublime-keymap
 	wget https://raw.githubusercontent.com/kennyhlam/config/master/Preferences.sublime-settings
 	wget https://raw.githubusercontent.com/kennyhlam/config/master/GitGutter.sublime-settings
-	wget https://raw.githubusercontent.com/kennyhlam/config/master/Default%20(Linux).sublime-keymap
+	wget "https://raw.githubusercontent.com/kennyhlam/config/master/Default%20(Linux).sublime-keymap"
 
 	# maybes
 	# Side​Bar​Enhancements, https://github.com/titoBouzout/SideBarEnhancements
