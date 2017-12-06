a3aa6e02dc84dbd0de236f30c7f1f02f2cf3d67f
Kenny Lam <khl1392@gmail.com>
2017-12-06 18:32:46 -0500

    updates, CUDA, cudnn

--- config.sh
+++ config.sh
@@ -12,7 +12,7 @@
 ln -sf $(readlink -f .gitconfig) ~/.gitconfig
 ln -sf $(readlink -f .vimrc) ~/.vimrc
 
-sudo apt-get install -y zsh
+sudo apt install -y zsh
 chsh -s `which zsh`
 ln -sf $(readlink -f .zshrc) ~/.zshrc
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -21,12 +21,19 @@ cp klam.zsh-theme ~/.oh-my-zsh/themes/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # :PluginInstall
 
-sudo apt-get install -y tree
+sudo apt install -y tree
 # install postgres, redis, etc
 
 
+sudo apt install -y dkms
 # install CUDA
 # https://askubuntu.com/questions/799184/how-can-i-install-cuda-on-ubuntu-16-04
+# uninstall CUDA
+# https://devtalk.nvidia.com/default/topic/994466/how-to-uninstall-cuda-toolkit-and-cudnn-under-linux-/
+echo "export PATH=/usr/local/cuda/bin:$PATH" >> ~/.bashrc
+echo "export PATH=/usr/local/cuda/bin:$PATH" >> ~/.zshrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.zshrc
 
 # install CuDNN
 # https://askubuntu.com/questions/767269/how-can-i-install-cudnn-on-ubuntu-16-04
--- python/python.sh
+++ python/python.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+sudo apt install -y python3-dev
 sudo apt install -y python3-pip
 pip3 install virtualenv
 pip3 install --upgrade pip
 virtualenv -p python3 ~/mypy3
 echo "\nsource ~/mypy3/bin/activate" >> ~/.bashrc
 echo "\nsource ~/mypy3/bin/activate" >> ~/.zshrc
+source ~/mypy3/bin/activate
+
+# pytorch
+#pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
+#pip3 install torchvision
+
+# tensorflow
+export CUDA_HOME=/usr/local/cuda
+sudo apt install -y libcupti-dev
+# easy-mode
+# pip3 install tensorflow-gpu
+
+# keras
+# pip3 install keras
+
+pip3 install -r requirements.txt
